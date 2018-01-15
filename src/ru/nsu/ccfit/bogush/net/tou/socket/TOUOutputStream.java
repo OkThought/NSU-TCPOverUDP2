2b48977b49d8fcdd1bfca0cf7743612ba1d947d8
RoyPy <bogush.vano@gmail.com>
2018-01-15 14:40:17 +0700

    Implement TOUOutputStream

--- src/ru/nsu/ccfit/bogush/net/tou/socket/TOUOutputStream.java
+++ src/ru/nsu/ccfit/bogush/net/tou/socket/TOUOutputStream.java
@@ -4,15 +4,48 @@ import java.io.IOException;
 import java.io.OutputStream;
 
 public class TOUOutputStream extends OutputStream {
-    private TOUSocketImpl impl;
+    private static final int BUFFER_SIZE = 1<<10;
 
-    public TOUOutputStream(TOUSocketImpl impl) {
+    private TOUSocketImpl impl;
+    private int seq;
+    private byte[] buffer = new byte[BUFFER_SIZE];
+    private int pos = 0;
+
+    TOUOutputStream(TOUSocketImpl impl) {
         this.impl = impl;
+        seq = impl.getInitialWriteSEQ();
     }
 
     @Override
-    public void write(int b)
+    public synchronized void write(int b)
             throws IOException {
+        if (impl.isClosedOrPending()) throw new IOException("Socket closed");
 
+        while (pos == buffer.length) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                throw new IOException(e);
+            }
+        }
+        buffer[pos] = (byte) b;
+        ++pos;
+    }
+
+    synchronized int available() {
+        return pos;
+    }
+
+    synchronized int getCurrentSEQ() {
+        return seq;
+    }
+
+    synchronized byte[] flushBytes() {
+        byte[] result = new byte[pos];
+        System.arraycopy(buffer, 0, result, 0, pos);
+        pos = 0;
+        ++seq;
+        notifyAll();
+        return result;
     }
 }
