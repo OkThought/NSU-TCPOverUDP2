0981d61ad39c3affb664faf5eb31e2d51b0cb124
RoyPy <bogush.vano@gmail.com>
2018-01-18 11:19:44 +0700

    Update TCP segment structure: - add data size field (int) - add data offset field (byte) to allow header extension
    Fixes:
    - fix a bug in calculating the capacity

--- src/ru/nsu/ccfit/bogush/net/tcp/segment/TCPSegment.java
+++ src/ru/nsu/ccfit/bogush/net/tcp/segment/TCPSegment.java
@@ -6,22 +6,39 @@ import java.util.Arrays;
 /**
  * TCPSegment structure:
  * <pre><code>
- *      +--------------------------------------------+
- *      |0|1|2|...7|8  ...  31|32       ...        63|
- *      |   sequence number   |      ack number      |
- *      |S|A|F|0...|data ...                         |
- *      +--------------------------------------------+
+ * +---------+-----+-----------------------+-----------------------+-----------------------+-----------------------+
+ * | offsets |octet|           0           |           1           |           2           |           3           |
+ * +---------+-----+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+ * |  octet  |byte | 0| 1| 2| 3| 4| 5| 6| 7| 8| 9|10|11|12|13|14|15|16|17|18|19|20|21|22|23|24|25|26|27|28|29|30|31|
+ * +---------+-----+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+ * |    0    |  0  |                                        sequence number                                        |
+ * +---------+-----+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+ * |    4    |  32 |                                           ack number                                          |
+ * +---------+-----+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+ * |    8    |  64 |                                           data size                                           |
+ * +---------+-----+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+ * |         |     |                       | A| S| F|              |                                               |
+ * |    12   |  96 |       data offset     | C| Y| I|    zeros     |                      data                     |
+ * |         |     |                       | K| N| N|              |        (starting at data offset octet)        |
+ * +---------+-----+-----------------------+--+--+--+--------------+-----------------------------------------------+
+ * |   ...   | ... |                                              ...                                              |
+ * +---------+-----+-----------------------------------------------------------------------------------------------+
  * </code></pre>
  */
 public class TCPSegment {
-    private static final int SEQ = 0;
-    private static final int ACK = SEQ + 4;
-    private static final int FLAGS = ACK + 4;
+    private static int off = 0;
+    private static int len = 0;
+    private static final int SEQ                = off = off + len; static {len = 4;}
+    private static final int ACK                = off = off + len; static {len = 4;}
+    private static final int DATA_SIZE          = off = off + len; static {len = 4;}
+    private static final int DATA_OFFSET        = off = off + len; static {len = 1;}
+    private static final int FLAGS              = off = off + len; static {len = 1;}
 
-    public static final int HEADER_SIZE = FLAGS + 1;
-    public static final byte ACK_BITMAP = (byte) 0b10000000;
-    public static final byte SYN_BITMAP = (byte) 0b01000000;
-    public static final byte FIN_BITMAP = (byte) 0b00100000;
+    private static final int DATA_OFFSET_MIN    = off;
+    public static final int HEADER_SIZE         = DATA_OFFSET_MIN + 1;
+    public static final byte ACK_BITMAP         = (byte) 0b10000000;
+    public static final byte SYN_BITMAP         = (byte) 0b01000000;
+    public static final byte FIN_BITMAP         = (byte) 0b00100000;
 
     private ByteBuffer buffer;
     private byte[] bytes;
@@ -33,6 +50,7 @@ public class TCPSegment {
     public TCPSegment(int capacity) {
         this(new byte[capacity + HEADER_SIZE]);
         Arrays.fill(bytes, (byte) 0);
+        setDataOffset(DATA_OFFSET_MIN);
     }
 
     public TCPSegment(TCPSegment other) {
@@ -98,8 +116,18 @@ public class TCPSegment {
         return this;
     }
 
+    public int getDataOffset() {
+        return buffer.get(DATA_OFFSET) & 0xff;
+    }
+
+    public TCPSegment setDataOffset(int dataOffset) {
+        buffer.put(DATA_OFFSET, (byte) dataOffset);
+        return this;
+    }
+
     public byte[] getData() {
-        return Arrays.copyOfRange(bytes, HEADER_SIZE, bytes.length);
+        int offset = getDataOffset();
+        return Arrays.copyOfRange(bytes, offset, offset + dataSize());
     }
 
     public TCPSegment setData(byte[] data) {
@@ -130,11 +158,11 @@ public class TCPSegment {
     }
 
     public int capacity() {
-        return bytes.length;
+        return bytes.length - getDataOffset();
     }
 
     public int dataSize() {
-        return bytes.length - HEADER_SIZE;
+        return buffer.getChar(DATA_SIZE);
     }
 
     public String flagsToString() {
