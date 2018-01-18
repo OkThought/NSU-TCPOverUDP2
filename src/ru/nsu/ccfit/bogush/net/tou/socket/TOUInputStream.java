db21f3a231d2a74a77181ec1f10363202d467f5d
RoyPy <bogush.vano@gmail.com>
2018-01-18 18:50:06 +0700

    Implement close() and setEof() in TOUInputStream

--- src/ru/nsu/ccfit/bogush/net/tou/socket/TOUInputStream.java
+++ src/ru/nsu/ccfit/bogush/net/tou/socket/TOUInputStream.java
@@ -8,24 +8,44 @@ public class TOUInputStream extends InputStream {
     private int seq;
     private byte[] data;
     private int pos = 0;
+    private boolean eof = false;
 
     TOUInputStream(TOUSocketImpl impl) {
         this.impl = impl;
         seq = impl.getInitialReadSEQ();
     }
 
+    private boolean closing = false;
+    public void close() throws IOException {
+        if (closing)
+            return;
+        closing = true;
+        if (!impl.isClosedOrPending()) {
+            impl.close();
+        }
+        closing = false;
+    }
+
+    void setEof(boolean eof) {
+        this.eof = eof;
+    }
+
     @Override
     public synchronized int read()
             throws IOException {
         if (data == null || pos == data.length) {
+            if (eof) return -1;
+            if (impl.isInShut()) throw new IOException("Input shutdown");
             try {
                 data = impl.fetchData(seq);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-                return -1;
-            }
+                if (data == null) {
+                    eof = true;
+                    return -1;
+                }
+            } catch (InterruptedException e) { return -1; }
             pos = 0;
         }
+
         return data[pos++];
     }
 }
